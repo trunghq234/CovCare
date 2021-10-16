61664a902c8152a540bb847e23d76997c4b840f7
Đỗ Văn Bảo <55795855+baodv1001@users.noreply.github.com>
2021-10-16 19:09:21 +0700

    Dev UI (Treatment & FAQ)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,14 @@
+import FAQ from 'components/Home/FAQ';
 import Prevention from 'components/Home/Prevention';
+import Treatment from 'components/Home/Treatment';
 import React from 'react';
 
 const AppHome = () => {
   return (
     <div>
       <Prevention />
+      <Treatment />
+      <FAQ />
     </div>
   );
 };
