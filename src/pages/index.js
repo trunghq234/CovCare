845b691406ea71b32937a55a5f122e265585ac03
anhtt2211 <trananh22112001@gmail.com>
2021-11-21 20:44:39 +0700

    fix file name to deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 export { default as AppHome } from './Home/index';
-export { default as DoctorPage } from './Doctor/Doctor';
-export { default as DoctorDetailPage } from './DoctorDetail/DoctorDetail';
+export { default as DoctorPage } from './Doctor';
+export { default as DoctorDetailPage } from './DoctorDetail';
